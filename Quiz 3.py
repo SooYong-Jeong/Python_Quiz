ee87044823f07e47a8cdf1237f45753bf8108281
SOOYONG <76885280+SooYong-Jeong@users.noreply.github.com>
2021-05-12 02:12:30 +0900

    Quiz3

--- Quiz 3.py
+++ Quiz 3.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+
 resi_num = input("주민등록번호를 입력하여 주세요 ex) 111111-1111111 : ")
 
-print(resi_num[8])
+if resi_num[8] == 1 or 2:
+    age = datetime.today().year - (1900 + int(resi_num[0:2]))
+elif resi_num[8] == 3 or 4:
+    age = datetime.today().year - (2000 + int(resi_num[0:2]))
+
+if (datetime.today().month - int(resi_num[2:4])) <= 0 and (datetime.today().day - int(resi_num[4:6])) < 0:
+    age -= 1
+
+print(f"만 {age}세")
