eeb2e0e13e55c0559ec7f0892e7dff64c60ea8f3
SOOYONG <76885280+SooYong-Jeong@users.noreply.github.com>
2021-05-12 23:12:24 +0900

    Quiz 6

---
+++ Quiz 6.py
@@ -0,0 +1,16 @@
+from random import *
+sub = 0
+ans = randrange(1, 101)
+print(ans)
+count = 0
+while True:
+    count += 1
+    sub = input("1~100사이 숫자를 입력하세요")
+    print(sub)
+    if int(sub) < ans:   
+        print(f"{count}번째 시도 : 더 큰수를 입력하세요")
+    elif int(sub) > ans:   
+        print(f"{count}번째 시도 : 더 작은수를 입력하세요")
+    elif int(sub) == ans:
+        break
+print(f"{count}번째 시도 정답")
